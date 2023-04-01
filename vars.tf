f7522557d0204a33a59de71ff01265138e2380f5
TARAK <tarakaramtirumala@gmail.com>
2023-04-01 07:54:12 +0530

    changes

--- vars.tf
+++ vars.tf
@@ -2,3 +2,7 @@ variable "env" {}
 variable "tags" {}
 variable "subnet_ids" {}
 variable "instance_type" {}
+variable "allow_subnets" {}
+variable "vpc_id" {}
+variable "bastion_cidr" {}
+variable "dns_domain" {}
