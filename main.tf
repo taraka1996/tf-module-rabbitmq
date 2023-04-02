054dee9ae8fd7d53899b2f988a4adda4136680a2
TARAK <tarakaramtirumala@gmail.com>
2023-04-02 20:24:37 +0530

    changes

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ resource "aws_spot_instance_request" "rabbitmq" {
   ami                  = data.aws_ami.ami.id
   instance_type        = var.instance_type
   subnet_id            = var.subnet_ids[0]
-  wait_for_fulfillment = true
   vpc_security_group_ids = [aws_security_group.main.id]
   iam_instance_profile   = aws_iam_instance_profile.main.name
 
